0bcba4fc6f7141967ba5e36b66b262ef5b1e7e28
xikiotaka <flowazcoaga@gmail.com>
2025-08-07 20:36:49 -0300

    Mejora del estilo de los campos en el formulario de contacto: se agregaron clases para un mejor diseño y accesibilidad.

--- blog/apps/contacto_admin/forms.py
+++ blog/apps/contacto_admin/forms.py
@@ -6,8 +6,18 @@ class ContactoForm(forms.ModelForm):
         model = Contacto
         fields = ['nombre_apellido', 'es_usuario', 'correo', 'tipo_consulta', 'mensaje']
         widgets = {
-            'nombre_apellido': forms.TextInput(attrs={'class': 'w-full px-3 py-2 border border-[#605dff] rounded'}),
-            'correo': forms.EmailInput(attrs={'class': 'w-full px-3 py-2 border border-[#605dff] rounded'}),
-            'tipo_consulta': forms.Select(attrs={'class': 'w-full px-3 py-2 border border-[#605dff] rounded'}),
-            'mensaje': forms.Textarea(attrs={'rows': 5, 'maxlength': 400, 'class': 'w-full px-3 py-2 border border-[#605dff] rounded'}),
+            'nombre_apellido': forms.TextInput(attrs={
+                'class': 'w-full px-3 py-2 border border-[#605dff] rounded bg-gray-800 text-white placeholder-gray-400'
+            }),
+            'correo': forms.EmailInput(attrs={
+                'class': 'w-full px-3 py-2 border border-[#605dff] rounded bg-gray-800 text-white placeholder-gray-400'
+            }),
+            'tipo_consulta': forms.Select(attrs={
+                'class': 'w-full px-3 py-2 border border-[#605dff] rounded bg-gray-800 text-white'
+            }),
+            'mensaje': forms.Textarea(attrs={
+                'rows': 5,
+                'maxlength': 400,
+                'class': 'w-full px-3 py-2 border border-[#605dff] rounded bg-gray-800 text-white placeholder-gray-400'
+            }),
         }
